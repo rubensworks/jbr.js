df5d09486bbd9ad20abdadaeb87b93ed07d32540
RubenEschauzier <ruben.eschauzier@gmail.com>
2026-05-14 14:58:50 +0200

    Reuse parts of SolidBench to reduce code duplication

--- packages/experiment-solid-session-bench/lib/ExperimentSolidSessionBench.ts
+++ packages/experiment-solid-session-bench/lib/ExperimentSolidSessionBench.ts
@@ -1,10 +1,7 @@
 import * as Path from 'path';
-import * as v8 from 'v8';
 import * as fs from 'fs-extra';
-import type { Experiment, Hook, ITaskContext,
-  DockerResourceConstraints, ICleanTargets, DockerContainerHandler, DockerNetworkHandler } from 'jbr';
-import { HttpAvailabilityLatch, ProcessHandlerComposite, secureProcessHandler } from 'jbr';
-import { Generator } from 'solidbench/lib/Generator';
+import type { ITaskContext } from 'jbr';
+import { ProcessHandlerComposite, secureProcessHandler } from 'jbr';
 import type {
   IResultSerializer,
 } from 'sparql-benchmark-runner';
@@ -14,116 +11,15 @@ import {
   ResultSerializerCsv,
   ResultSerializerRaw,
 } from 'sparql-benchmark-runner';
+import type { IExperimentSolidBenchOptions } from '../../experiment-solidbench';
+import { ExperimentSolidBench } from '../../experiment-solidbench';
 
 /**
  * An experiment instance for the SolidBench social network benchmark.
  */
-export class ExperimentSolidSessionBench implements Experiment {
-  public readonly httpAvailabilityLatch = new HttpAvailabilityLatch();
-  public readonly scale: string;
-  public readonly configEnhance: string;
-  public readonly configFragment: string;
-  public readonly configQueries: string;
-  public readonly configServer: string;
-  public readonly validationParamsUrl: string | undefined;
-  public readonly configValidation: string | undefined;
-  public readonly hadoopMemory: string;
-  public readonly dockerfileServer: string;
-  public readonly hookSparqlEndpoint: Hook;
-  public readonly serverPort: number;
-  public readonly serverLogLevel: string;
-  public readonly serverBaseUrl: string;
-  public readonly serverResourceConstraints: DockerResourceConstraints;
-  public readonly endpointUrl: string;
-  public readonly queryRunnerReplication: number;
-  public readonly queryRunnerWarmupRounds: number;
-  public readonly queryRunnerRequestDelay: number;
-  public readonly queryRunnerEndpointAvailabilityCheckTimeout: number;
-  public readonly queryRunnerUrlParams: Record<string, any>;
-  public readonly queryTimeoutFallback: number | undefined;
-
+export class ExperimentSolidSessionBench extends ExperimentSolidBench {
   public constructor(options: IExperimentSolidBenchOptions) {
-    this.scale = options.scale;
-    this.configEnhance = options.configEnhance;
-    this.configFragment = options.configFragment;
-    this.configQueries = options.configQueries;
-    this.configServer = options.configServer;
-    this.validationParamsUrl = options.validationParamsUrl;
-    this.configValidation = options.configValidation;
-    this.hadoopMemory = options.hadoopMemory;
-    this.dockerfileServer = options.dockerfileServer;
-    this.hookSparqlEndpoint = options.hookSparqlEndpoint;
-    this.endpointUrl = options.endpointUrl;
-    this.serverPort = options.serverPort;
-    this.serverLogLevel = options.serverLogLevel;
-    this.serverBaseUrl = options.serverBaseUrl;
-    this.serverResourceConstraints = options.serverResourceConstraints;
-    this.queryRunnerReplication = options.queryRunnerReplication;
-    this.queryRunnerWarmupRounds = options.queryRunnerWarmupRounds;
-    this.queryRunnerRequestDelay = options.queryRunnerRequestDelay;
-    this.queryRunnerEndpointAvailabilityCheckTimeout = options.queryRunnerEndpointAvailabilityCheckTimeout;
-    this.queryRunnerUrlParams = options.queryRunnerUrlParams;
-    this.queryTimeoutFallback = options.queryTimeoutFallback;
-  }
-
-  public getDockerImageName(context: ITaskContext, type: string): string {
-    return context.docker.imageBuilder.getImageName(context, `solidbench-${type}`);
-  }
-
-  public async replaceBaseUrlInDir(path: string): Promise<void> {
-    for (const entry of await fs.readdir(path, { withFileTypes: true })) {
-      if (entry.isFile()) {
-        const file = Path.join(path, entry.name);
-        await fs.writeFile(file, (await fs.readFile(file, 'utf8'))
-          .replace(/localhost:3000/ug, 'solidbench-server:3000'));
-      } else if (entry.isDirectory()) {
-        await this.replaceBaseUrlInDir(Path.join(path, entry.name));
-      }
-    }
-  }
-
-  public async prepare(context: ITaskContext, forceOverwriteGenerated: boolean): Promise<void> {
-    // Validate memory limit
-    const minimumMemory = 8192;
-    // eslint-disable-next-line no-bitwise
-    const currentMemory = v8.getHeapStatistics().heap_size_limit / 1024 / 1024;
-    if (currentMemory < minimumMemory) {
-      context.logger.warn(`SolidBench recommends allocating at least ${minimumMemory} MB of memory, while only ${currentMemory} was allocated.\nThis can be configured using Node's --max_old_space_size option.`);
-    }
-
-    // Prepare hook
-    await this.hookSparqlEndpoint.prepare(context, forceOverwriteGenerated);
-
-    // Prepare dataset
-    await new Generator({
-      verbose: context.verbose,
-      cwd: context.experimentPaths.generated,
-      overwrite: forceOverwriteGenerated,
-      scale: this.scale,
-      enhancementConfig: Path.resolve(context.cwd, this.configEnhance),
-      fragmentConfig: Path.resolve(context.cwd, this.configFragment),
-      queryConfig: Path.resolve(context.cwd, this.configQueries),
-      validationParams: this.validationParamsUrl,
-      validationConfig: this.configValidation ? Path.resolve(context.cwd, this.configValidation) : undefined,
-      hadoopMemory: this.hadoopMemory,
-    }).generate();
-
-    // Replace prefix URLs to correct base URL in queries directory
-    await this.replaceBaseUrlInDir(Path.resolve(context.experimentPaths.generated, 'out-queries'));
-
-    // Build server Dockerfile
-    await context.docker.imageBuilder.build({
-      cwd: context.experimentPaths.root,
-      dockerFile: this.dockerfileServer,
-      auxiliaryFiles: [ this.configServer ],
-      imageName: this.getDockerImageName(context, 'server'),
-      buildArgs: {
-        CONFIG_SERVER: this.configServer,
-        LOG_LEVEL: this.serverLogLevel,
-        BASE_URL: this.serverBaseUrl,
-      },
-      logger: context.logger,
-    });
+    super(options);
   }
 
   public async run(context: ITaskContext): Promise<void> {
@@ -196,76 +92,4 @@ export class ExperimentSolidSessionBench implements Experiment {
     // Close endpoint and server
     await closeProcess();
   }
-
-  public async startServer(context: ITaskContext): Promise<[ DockerContainerHandler, DockerNetworkHandler ] > {
-    // Create shared network
-    const networkHandler = await context.docker.networkCreator
-      .create({ Name: this.getDockerImageName(context, 'network') });
-    const network = networkHandler.network.id;
-
-    // Ensure logs directory exists
-    await fs.ensureDir(Path.join(context.experimentPaths.output, 'logs'));
-
-    const filePath = this.serverBaseUrl.replace('://', '/').replace(':', '_');
-    const serverHandler = await context.docker.containerCreator.start({
-      containerName: 'solidbench-server',
-      imageName: this.getDockerImageName(context, 'server'),
-      resourceConstraints: this.serverResourceConstraints,
-      hostConfig: {
-        Binds: [
-          `${Path.join(context.experimentPaths.generated, `out-fragments/${filePath}`)}/:/data`,
-        ],
-        PortBindings: {
-          '3000/tcp': [
-            { HostPort: `${this.serverPort}` },
-          ],
-        },
-        NetworkMode: network,
-      },
-      logFilePath: Path.join(context.experimentPaths.output, 'logs', 'server.txt'),
-      statsFilePath: Path.join(context.experimentPaths.output, 'stats-server.csv'),
-    });
-
-    return [ serverHandler, networkHandler ];
-  }
-
-  public async clean(context: ITaskContext, cleanTargets: ICleanTargets): Promise<void> {
-    await this.hookSparqlEndpoint.clean(context, cleanTargets);
-
-    if (cleanTargets.docker) {
-      await context.docker.networkCreator.remove(this.getDockerImageName(context, 'network'));
-      await context.docker.containerCreator.remove('solidbench-server');
-    }
-  }
-
-  public async waitForEndpoint(context: ITaskContext): Promise<void> {
-    await this.httpAvailabilityLatch.sleepUntilAvailable(context, `${this.serverBaseUrl}dbpedia.org/`);
-  }
-}
-
-export interface IExperimentSolidBenchOptions {
-  scale: string;
-  configEnhance: string;
-  configFragment: string;
-  configQueries: string;
-  configServer: string;
-  validationParamsUrl?: string;
-  configValidation?: string;
-  hadoopMemory: string;
-  dockerfileServer: string;
-  hookSparqlEndpoint: Hook;
-  serverPort: number;
-  serverLogLevel: string;
-  serverBaseUrl: string;
-  serverResourceConstraints: DockerResourceConstraints;
-  endpointUrl: string;
-  queryRunnerReplication: number;
-  queryRunnerWarmupRounds: number;
-  queryRunnerRequestDelay: number;
-  queryRunnerEndpointAvailabilityCheckTimeout: number;
-  /**
-   * @range {json}
-   */
-  queryRunnerUrlParams: Record<string, any>;
-  queryTimeoutFallback?: number;
 }
